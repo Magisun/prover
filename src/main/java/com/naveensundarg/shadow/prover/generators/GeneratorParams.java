bb633d0e42ed1ea5c6da4b5a1a5d12b52f013faa
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2018-05-29 07:19:25 -0400

    Fix formatting in GeneratorParams

--- src/main/java/com/naveensundarg/shadow/prover/generators/GeneratorParams.java
+++ src/main/java/com/naveensundarg/shadow/prover/generators/GeneratorParams.java
@@ -6,35 +6,35 @@ import java.lang.reflect.Field;
 
 public class GeneratorParams {
 
-        public int maxAtoms;
-        public int maxLiteralsInClause;
-        // TODO: Make into a range
-        public int clauses;
+    public int maxAtoms;
+    public int maxLiteralsInClause;
+    // TODO: Make into a range
+    public int clauses;
 
 
-        public GeneratorParams copy() {
-            GeneratorParams params = new GeneratorParams();
+    public GeneratorParams copy() {
+        GeneratorParams params = new GeneratorParams();
 
-            copyTo(params);
+        copyTo(params);
 
-            return params;
+        return params;
+    }
+
+    protected void copyTo(GeneratorParams other) {
+        other.maxAtoms = this.maxAtoms;
+        other.maxLiteralsInClause = this.maxLiteralsInClause;
+        other.clauses = this.clauses;
+    }
+
+    public JSONObject toJSON() {
+        Field[] fields = this.getClass().getFields();
+        String[] fieldNames = new String[fields.length];
+
+        for (int i = 0; i < fields.length; i++) {
+            fieldNames[i] = fields[i].getName();
         }
 
-        protected void copyTo(GeneratorParams other) {
-            other.maxAtoms = this.maxAtoms;
-            other.maxLiteralsInClause = this.maxLiteralsInClause;
-            other.clauses = this.clauses;
-        }
-
-        public JSONObject toJSON() {
-            Field[] fields = this.getClass().getFields();
-            String[] fieldNames = new String[fields.length];
-
-            for(int i = 0; i < fields.length; i++) {
-                fieldNames[i] = fields[i].getName();
-            }
-
-            return new JSONObject(this, fieldNames);
-        }
- }
+        return new JSONObject(this, fieldNames);
+    }
+}
 
