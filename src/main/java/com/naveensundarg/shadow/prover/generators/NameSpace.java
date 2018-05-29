3574e37a1f94679aeb324741721156f3cb821bdf
Jean-Claude Paquin <jcrpaquin@yahoo.com>
2018-05-29 08:18:20 -0400

    Update NameSpace

--- src/main/java/com/naveensundarg/shadow/prover/generators/NameSpace.java
+++ src/main/java/com/naveensundarg/shadow/prover/generators/NameSpace.java
@@ -7,14 +7,20 @@ import java.util.Map;
 public class NameSpace {
 
     private String prefix;
-    private int cur_index;
-    private Map<String, Integer> name_map;
+    private int curIndex;
+    private Map<String, Integer> nameMap;
 
 
     public NameSpace(String prefix) {
         this.prefix = prefix;
-        this.cur_index = 1;
-        this.name_map = CollectionUtils.newMap();
+        this.curIndex = 1;
+        this.nameMap = CollectionUtils.newMap();
+    }
+
+    public NameSpace(String prefix, int startAt) {
+        this(prefix);
+
+        this.curIndex = startAt;
     }
 
 
@@ -22,15 +28,24 @@ public class NameSpace {
         StringBuilder builder = new StringBuilder();
 
         builder.append(prefix);
-        builder.append(Integer.toString(this.cur_index));
+        builder.append(Integer.toString(this.curIndex));
 
         String name = builder.toString();
-        name_map.put(name, this.cur_index++);
+        nameMap.put(name, this.curIndex++);
 
         return name;
     }
 
-    public Integer getNameIndex(String name) {
-        return name_map.get(name);
+    public Integer nameToIndex(String name) {
+        if(!name.startsWith(prefix)) {
+            throw new IllegalArgumentException("Specified string does not start with this" +
+                    " NameSpace's prefix (" + prefix + ")");
+        }
+
+        return nameMap.get(name);
+    }
+
+    public String indexToName(Integer index) {
+        return prefix + index;
     }
 }
